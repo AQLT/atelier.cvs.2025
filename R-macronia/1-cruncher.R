8433e5e71ba7e08e6f2aa0ff1869b7ec528f9de7
AQLT <alain.quartier@yahoo.fr>
2025-08-20 15:40:18 +0200

    commentaires

--- R-macronia/1-cruncher.R
+++ R-macronia/1-cruncher.R
@@ -17,18 +17,25 @@ getOption("cruncher_bin_directory")
 
 # Plutôt que de lancer le cruncher directement sur le workspace on va faire une copie
 library(rjd3workspace)
-jws <- jws_open("data/macronia.xml")
+jws <- jws_open(
+  # Chemin vers le workspace, A MODIFIER POUR POINTER VERS VOTRE WORKSPACE
+  "data/macronia.xml"
+)
 new_file_workspace <- sprintf(
-  "R-macronia/macronia_%s.xml",
+  "R-macronia/macronia_%s.xml", # A MODIFIER
   format(Sys.time(), # je récupère la date du jour
          "%m_%Y") # je la mets sous le format MM_YYYY
 )
 # Si plus simple mettre directement le nom du fichier :
-new_file_workspace <- "R-macronia/macronia_08_2025.xml"
+new_file_workspace <- "R-macronia/macronia_08_2025.xml" # MODIFIER
 
+# Ce code permet de créer la copie du workspace
 rjd3workspace::save_workspace(jws, new_file_workspace, replace = TRUE)
+
+# ATTENTION : SI VOUS UTILISEZ DIRECTEMENT LE WORKSPACE macronia.xml AUCUNE DONNÉE NE SERA EXPORTÉE :
+# LE CRUNCHER VA EN EFFET CHERCHER LES DONNÉES LÀ OÙ J'AI CRÉÉ LE PROGRAMME
 cruncher(workspace = new_file_workspace,
-         param_file_path = "R-macronia/parameters.param",
+         param_file_path = "R-macronia/parameters.param", # A MODIFIER AVEC VOTRE CHEMIN
          log_file = "log.txt"
 )
 
@@ -36,10 +43,18 @@ cruncher(workspace = new_file_workspace,
 ##################################
 ### Récupération des résultats ###
 ##################################
+### Cette partie est optionnelle : elle permet juste d'avoir des codes exemples pour importer des données
 # Méthode 1 : récupérer les données
 library(readr)
 series_sa <- read_delim(
-  file.path(gsub(".xml", "", new_file_workspace), "Output", "SAProcessing-1", "series_sa.csv"),
+  # On spécifie ici le chemin vers le fichier
+  # Par défaut, il sera dans le dossier associé à votre workspace, dans un sous-dossier output
+  # et ensuite vous aurez un dossier par SAProcessing
+  file.path(
+    gsub(".xml", "", new_file_workspace), 
+    "Output", 
+    "SAProcessing-1", # On ne prend que les fichiers associés au premier SAProcessing, a modifier éventuellement
+    "series_sa.csv"),
   delim = ";", escape_double = FALSE, col_names = TRUE,
   # Si sous JDemetra+ les données importées depuis un fichier Excel avec des accents,
   # il peut être nécessaire de spécifier l'encodage
@@ -53,7 +68,14 @@ series_sa <- as.data.frame(series_sa)
 XLConnect::writeWorksheetToFile("series_y.xlsx", series_sa, sheet = "y")
 # Autre façon de lire les données :
 series_sa2 <- read.csv2(
-  file.path(gsub(".xml", "", new_file_workspace), "Output", "SAProcessing-1", "series_sa.csv"),,
+  # On spécifie ici le chemin vers le fichier
+  # Par défaut, il sera dans le dossier associé à votre workspace, dans un sous-dossier output
+  # et ensuite vous aurez un dossier par SAProcessing
+  file.path(
+    gsub(".xml", "", new_file_workspace), 
+    "Output", 
+    "SAProcessing-1", # On ne prend que les fichiers associés au premier SAProcessing, a modifier éventuellement
+    "series_sa.csv"),
   fileEncoding = "WINDOWS-1252"
 )
 
@@ -67,12 +89,10 @@ all_jmod <- lapply(all_jmod, function(sap) {
   sap
 })
 
+# Permet d'avoir une liste avec les séries désaisonnalisées de chaque SAProcessing
 all_sa<- lapply(all_jmod, function(mp) {
   do.call(ts.union, lapply(mp, ggdemetra3::seasonaladj))
 })
 all_y<- lapply(all_jmod, function(mp) {
   do.call(ts.union, lapply(mp, ggdemetra3::raw))
 })
-s_trim <- rjd3toolkit::aggregate(all_y$IHPI[,1], nfreq = 4, conversion = "Average")
-
-plot(s_trim)
